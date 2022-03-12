391fb377842bf7365d1d9f654584335bbaec38da
sagarpakhrin <sagarlama826@gmail.com>
2022-03-12 23:46:22 +0545

    feat: Add get users api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/users')
+  getUsers(): Promise<User[]> {
+    return this.appService.getUsers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 }
